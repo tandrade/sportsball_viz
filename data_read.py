336d602d934813cc987abf1ef14dc3dd1dde5665
tandrade <nos.tamaramos@gmail.com>
2018-06-04 22:28:11 -0500

    Add python and hockey files to git

---
+++ data_read.py
@@ -0,0 +1,16 @@
+'''
+A script to read in excel files about modern teams and output
+JSON data related to how long it had been since a team had been
+the champion.
+'''
+
+
+def main():
+    pass
+
+'''
+Data sources:
+
+Stanley cup winners: https://www.hockey-reference.com/awards/stanley.html
+Hockey history: https://www.nhl.com/news/nhl-expansion-history/c-281005106
+'''
